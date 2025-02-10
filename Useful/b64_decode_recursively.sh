5b6783ee39c932e0010baa529db0fb320f76d309
Luks <51793738+luksecurity@users.noreply.github.com>
2025-02-10 17:35:29 +0100

    Delete Useful/b64_decode_recursively.sh

--- Useful/b64_decode_recursively.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to recursively decode base64 encoded string
-
-state=$(<base64.txt)
-for i in {1..13}; do
-   state=$(<<<"$state" base64 --decode)
-   printf "\n%s\n------------" $state
-done
