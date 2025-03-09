a6bf694ff07fc42fd4f48a6e925027d57b2010b8
Luks <51793738+luksecurity@users.noreply.github.com>
2025-03-09 14:33:11 +0100

    Create password_guessing.py

---
+++ Useful/password_guessing.py
@@ -0,0 +1,17 @@
+import itertools
+
+base = "Motdepasse"
+
+def generate_case_permutations(word):
+    return set("".join(p) for p in itertools.product(*[(c.lower(), c.upper()) for c in word]))
+
+years = [str(y) for y in range(2017, 2026)]
+
+special_chars = "!@#$%^&*()-_=+<>?/"
+
+with open("password.txt", "w") as f:
+    for perm in generate_case_permutations(base):
+        for year, char in itertools.product(years, special_chars):
+            f.write(f"{perm}{year}{char}\n")
+
+print("Wordlist générée : password.txt")
